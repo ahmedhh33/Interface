375eb04c2c531ffd8da5bde523beadf37d0f64b4
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-21 13:17:32 +0400

    second commit

---
+++ Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacebehavior
+{
+    internal class Circle : Ishape
+    {
+        public double radius;
+        public Circle (double radius)
+        {
+            this.radius = radius;
+        }
+        public double calculateArea()
+        {
+            return Math.PI * Math.Pow(this.radius, 2);
+        }
+
+        public double getperimeter()
+        {
+            return 2 * Math.PI * this.radius;
+        }
+    }
+    internal class Rectangle : Ishape
+    {
+        public double lenght;
+        public double width;
+        public Rectangle(double lenght, double width)
+        {
+            this.lenght = lenght;
+            this.width = width;
+        }
+        public double calculateArea()
+        {
+            return this.lenght * this.width;
+        }
+
+        public double getperimeter()
+        {
+            return 2*(this.lenght + this.width);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Interfacebehavior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ishape circle = new Circle(4);
+            Console.WriteLine("The area of Circle = "+circle.calculateArea());
+            Console.WriteLine("The perimeter of Circle = "+circle.getperimeter());
+            Ishape rectangle = new Rectangle(2, 3);
+            Console.WriteLine("The area of Rectangle = " + rectangle.calculateArea());
+            Console.WriteLine("The perimeter of Reectangle = " + rectangle.getperimeter());
+        }
+
+    }
+}
